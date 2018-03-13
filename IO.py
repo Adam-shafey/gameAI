17285b5babc185c7ca294ade1d0e794e36cd816a
JAdamWeller <Jadamweller@gmail.com>
2018-03-13 02:34:00 -0400

    Add files via upload
    This IO has a second printResult method for the automated pathfinder

--- IO.py
+++ IO.py
@@ -2,11 +2,11 @@ class IO(object):
 
     @staticmethod
     def readInitial(fileName=None): #defaults to Untitled.txt
-        data=[]
+        data = []
         if fileName:
             filename=fileName
         else:
-            filename="Untitled.txt"
+            filename = "Untitled.txt"
         with open(filename, 'r') as fobj:
             for line in fobj:
                 lineData = [letter for letter in line.split()]
@@ -40,4 +40,34 @@ class IO(object):
             thefile.write("Number of Moves: " + str(numOfMoves[index]))
             thefile.write("\n")
             thefile.write("\n")
+        thefile.write("\n")
+
+    @staticmethod
+    def printResult2(solution, result, numOfMoves, *timeTaken):
+        thefile = open('Output.txt', 'w')
+        for index in range(len(solution)): #Loops through solutions
+            thefile.write("Puzzle: " + str(index + 1))
+            thefile.write("\n")
+            thefile.write("Solution: ")
+            for item in solution[index]:
+                thefile.write(item.capitalize())
+                thefile.write(' ')
+            thefile.write("\n")
+            thefile.write("Result: ")
+            for item in result[index]:
+                thefile.write(item.capitalize())
+                thefile.write(' ')
+            thefile.write("\n")
+            thefile.write("Number of Moves: " + str(numOfMoves[index]))
+            thefile.write("\n")
+            thefile.write("\n")
+        thefile.write("\n")
+        if (timeTaken):
+            thefile.write("\n" + "Time: ")
+            thefile.write("%s" % timeTaken + "ms")
+        thefile.write("\n")
+        sum = 0
+        for index in range(len(numOfMoves)):
+            sum = sum + numOfMoves[index]
+        thefile.write("Total Number of Moves: " + str(sum))
         thefile.write("\n")
